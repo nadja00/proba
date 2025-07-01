7ced0135efcadd021aae7e7d94eca959ab7f220c
nadja00 <nadja.jevremovic00@gmail.com>
2025-07-01 10:14:56 +0200

    function Razlika

--- vezba1/main.c
+++ vezba1/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+int Razlika(int x, int y) {
+    if (x > y)
+        return x - y;
+    else
+        return y - x;
+}
+
 int main() {
     int a = 5;
 	int b = 8;
 	int c;
 
-	if (a > b)
-		c = a - b;
-	else 
-		c = b - a;
+	c = Razlika(a,b);
+	
 	printf("Rezultat je: %d \n", c);
 	
 	return 0;
