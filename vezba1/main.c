94343bc40186bab0e39a1d564808d8bc88b62eea
nadja00 <nadja.jevremovic00@gmail.com>
2025-07-01 10:00:55 +0200

    initial code

--- vezba1/main.c
+++ vezba1/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int a = 5;
+	int b = 4;
+	int c;
+
+	if (a > b)
+		c = a - b;
+	else 
+		c = b - a;
+	printf("Rezultat je: %d \n", c);
+	
+	return 0;
 }
